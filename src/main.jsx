213f327f888279bbe47475a6e8d3f1198481f476
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-06-07 20:39:41 +0600

    creat the navbar and footer component

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import router from './Routes/router.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <div className='max-w-7xl mx-auto'>
+    <div className='max-w-7xl  mx-auto'>
       <RouterProvider router={router} />
     </div>
   </React.StrictMode>,
