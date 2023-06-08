4b86b2201037ccb2014997ad4c392f06f63d06f3
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-06-08 14:33:01 +0600

    added the web logo in navbr and footer

--- src/Pages/Home/Home/Shared/Navbar/Navbar.jsx
+++ src/Pages/Home/Home/Shared/Navbar/Navbar.jsx
@@ -1,39 +1,30 @@
-
+import { Link } from "react-router-dom";
+import logo from '../../../../../assets/logo.png'
 const Navbar = () => {
+
+    const navLink = <>
+        <li><Link>Home</Link></li>
+        <li><a>Item 3</a></li>
+    </>
+
+
     return (
-        <div className="navbar bg-stone-400">
+        <div className="navbar bg-stone-100 fixed max-w-7xl mx-auto">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <a>Parent</a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
+                        {navLink}
                     </ul>
                 </div>
-                <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                <img className="h-16 w-16" src={logo} alt="" />
+               <h2 className="text-2xl font-bold text-red-500">SADIQ</h2>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                    <li><a>Item 1</a></li>
-                    <li tabIndex={0}>
-                        <details>
-                            <summary>Parent</summary>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                    <li><a>Item 3</a></li>
+                    {navLink}
                 </ul>
             </div>
             <div className="navbar-end">
