748176e4519a0beae7c56ab61f781bc219a2084a
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-06-09 21:24:33 +0600

    update the contact number

--- src/Pages/Instructors/Instructors.jsx
+++ src/Pages/Instructors/Instructors.jsx
@@ -1,6 +1,9 @@
+import { Helmet } from "react-helmet";
+
 const Instructors = () => {
     return (
         <div>
+             <Helmet><title>Sadiq | instructores</title></Helmet>
             <h2> This is Instructors page</h2>
         </div>
     );
