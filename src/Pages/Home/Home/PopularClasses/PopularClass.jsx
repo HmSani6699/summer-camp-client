e3eb8e597e913f39647cf9c0afc9c811e0009acc
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-06-10 04:54:41 +0600

    creat the popolar class section

---
+++ src/Pages/Home/Home/PopularClasses/PopularClass.jsx
@@ -0,0 +1,26 @@
+
+const PopularClass = ({ classe }) => {
+    const { image,name, } = classe;
+
+
+    return (
+        <div className="card  w-full border bg-base-100 shadow-xl">
+            <div className="bg-slate-800 rounded-t-xl">
+                <figure><img className="h-[250px]" src={image} alt="Shoes" /></figure>
+            </div>
+            <div className="card-body ">
+                <div className="flex items-center justify-between">
+                    <div >
+                        <h2 className="card-title">{name}</h2>
+                        <p><span className="font-bold">Email:</span></p>
+                    </div>
+                    <div className="border bg-amber-300 h-8 w-8 rounded-full flex items-center justify-center">
+                        <button className="btn">Add</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default PopularClass;
--- src/Pages/Home/Home/PopularClasses/PopularClasses.jsx
+++ src/Pages/Home/Home/PopularClasses/PopularClasses.jsx
@@ -1,17 +1,29 @@
 import axios from "axios";
 import SectionTitle from "../../../../Component/SectioneTitle/SectionTitle";
+import { useState } from "react";
+import PopularClass from "./PopularClass";
 
 const PopularClasses = () => {
+    const[classes,setClasses]=useState()
 
     axios.get('http://localhost:5000/classes')
     .then(data=>{
-        console.log(data.data);
+        setClasses(data.data);
     })
 
     return (
         <div>
              <h2 className="text-4xl font-bold text-center mt-24">Our Popular <span className="text-amber-400">Classes</span></h2>
             <SectionTitle subHeadding={`We understand the importance of providing comprehensive instructions and guidance to your students. This section is designed specifically for PopularInstructors, offering additional resources and tips to enhance the learning experience.`}></SectionTitle>
+
+            <div>
+                {
+                    classes?.map(classe=><PopularClass 
+                    key={classe._id}
+                    
+                    ></PopularClass>)
+                }
+            </div>
             
         </div>
     );
