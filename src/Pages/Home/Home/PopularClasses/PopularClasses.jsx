d5f5cf8f4cc2533e64439457835286b25a26b9d1
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-06-10 04:47:35 +0600

    load the popular classes data

--- src/Pages/Home/Home/PopularClasses/PopularClasses.jsx
+++ src/Pages/Home/Home/PopularClasses/PopularClasses.jsx
@@ -1,6 +1,13 @@
+import axios from "axios";
 import SectionTitle from "../../../../Component/SectioneTitle/SectionTitle";
 
 const PopularClasses = () => {
+
+    axios.get('http://localhost:5000/classes')
+    .then(data=>{
+        console.log(data.data);
+    })
+
     return (
         <div>
              <h2 className="text-4xl font-bold text-center mt-24">Our Popular <span className="text-amber-400">Classes</span></h2>
@@ -11,3 +18,4 @@ const PopularClasses = () => {
 };
 
 export default PopularClasses;
+
