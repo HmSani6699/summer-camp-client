899c510cd846d115cfb412e8d4becdd5ebfbc26a
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-06-09 07:07:39 +0600

    creat the first bnner part

--- src/Pages/Home/Home/Banner/Banner.jsx
+++ src/Pages/Home/Home/Banner/Banner.jsx
@@ -3,7 +3,8 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-import logo from '../../../../assets/logo.png'
+import logo from '../../../../assets/logo.png';
+import banner from '../../../../assets/banner.png'
 
 const Banner = () => {
 
@@ -23,15 +24,26 @@ const Banner = () => {
             className="mySwiper"
         >
             <SwiperSlide>
-                <div className="bg-slate-800 px-20 py-20">
-                    <div className="flex items-center mb-6">
-                        <img className="h-16 w-16" src={logo} alt="" />
-                        <h2 className="text-white  text-2xl">We Will Teach You How To Learn</h2>
+                <div className="flex items-center justify-center bg-slate-800 px-20 pt-40 pb-20">
+                    <div className="">
+                        <div className="flex items-center mb-6">
+                            <img className="h-16 w-16" src={logo} alt="" />
+                            <h2 className="text-white  text-2xl">We Will Teach You How To Learn</h2>
+                        </div>
+                        <h2 className="text-white text-5xl font-bold">BEST SCHOOL & ANYTHING </h2><h2 className="text-white text-5xl font-bold mt-3"> TO <span className="text-amber-400">LEARN</span> SMART WAY</h2>
+                        <p className="text-white mt-8">A Theme for Kids , Learning & Course etc.</p>
+                        <input type="submit" className="btn bg-[#fbbc24e3] mr-5 border-0 mt-8" value="Get start" />
+                        <input type="submit" className="btn btn-outline btn-warning" value="Contact us" />
                     </div>
-                    <h2 className="text-white text-5xl font-bold">BEST SCHOOL & ANYTHING </h2><h2 className="text-white text-5xl font-bold mt-3"> TO <span className="text-amber-500">LEARN</span> SMART WAY</h2>
-                    <p className="text-white mt-8">A Theme for Kids , Learning & Course etc.</p>
-                    <input type="submit" className="btn bg-[#fbbc24e3] mr-5 border-0 mt-8" value="Get start" />
-                    <input type="submit" className="btn btn-outline btn-warning" value="Contact us" />
+                    <div className="border rounded-tl-[50%] border-white ml-12">
+                        <div className="avatar">
+                            <div className="bg-amber-400 rounded-full border-8">
+                                <span className="border-4 border-white">
+                                    <img className="" src={banner} />
+                                </span>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </SwiperSlide>
         </Swiper>
