8b61dc8b0ad409d6f34d05417b1d1573ad837b53
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-06-09 18:33:07 +0600

    get the review data

--- src/Pages/Home/Home/Feedback/Feedback.jsx
+++ src/Pages/Home/Home/Feedback/Feedback.jsx
@@ -9,16 +9,22 @@ import './Feedback.css'
 // import required modules
 import { Autoplay, EffectCoverflow, Pagination } from "swiper";
 import SectionTitle from "../../../../Component/SectioneTitle/SectionTitle";
+import axios from "axios";
 
 
 
 const Feedback = () => {
-  return (
-    <div className="max-w-6xl mx-auto">
-      <h2 className="text-4xl font-bold text-center mt-24">Our Client <span className="text-amber-400">Review</span></h2>
-      <SectionTitle subHeadding={`We understand the importance of providing comprehensive instructions and guidance to your students. This section is designed specifically for PopularInstructors, offering additional resources and tips to enhance the learning experience.`}></SectionTitle>
-      <div className=" mt-12 mb-24">
 
+
+  axios.get('http://localhost:5000/review')
+  .then(data => {
+    console.log(data.data);
+  })
+  return (
+    <div className="max-w-6xl mx-auto bg-[#f8f9fa] py-10 mt-24">
+      <h2 className="text-4xl font-bold text-center ">Our Client <span className="text-amber-400">Review</span></h2>
+      <SectionTitle subHeadding={`We understand the importance of providing comprehensive instructions and guidance to your students. This section is designed specifically for PopularInstructors, offering additional resources and tips to enhance the learning experience.`}></SectionTitle>
+      <div className=" mb-24 mt-10">
         <Swiper
           effect={"coverflow"}
           grabCursor={true}
@@ -40,9 +46,12 @@ const Feedback = () => {
           className="mySwiper"
         >
           <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+            <div className="bg-white">
+              <img src='' alt="" />
+              <h2 className="text-center">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores quibusdam tempora rerum amet earum dignissimos quo quia cumque exercitationem eligendi?</h2>
+            </div>
           </SwiperSlide>
-          <SwiperSlide>
+          {/* <SwiperSlide>
             <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
           </SwiperSlide>
           <SwiperSlide>
@@ -53,7 +62,7 @@ const Feedback = () => {
           </SwiperSlide>
           <SwiperSlide>
             <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
-          </SwiperSlide>
+          </SwiperSlide> */}
         </Swiper>
       </div>
     </div>
