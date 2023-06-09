b3ae7aeba218197c1a2719eb78769640760245ab
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-06-09 15:57:26 +0600

    creat the welcome ceation

---
+++ src/Pages/Home/Home/Welcome/Welcome.jsx
@@ -0,0 +1,14 @@
+import SectionTitle from "../../../../Component/SectioneTitle/SectionTitle";
+
+const Welcome = () => {
+    return (
+        <div>
+            <h2 className="text-4xl font-bold text-center mt-24">Welcome to <span className="text-amber-400">Sadiq</span></h2>
+            <SectionTitle subHeadding={`We understand the importance of providing comprehensive instructions and guidance to your students. This section is designed specifically for PopularInstructors, offering additional resources and tips to enhance the learning experience.`}></SectionTitle>
+
+            
+        </div>
+    );
+};
+
+export default Welcome;
---
+++ src/Pages/Home/Home/Welcome/WelcomeCart.jsx
@@ -0,0 +1,10 @@
+
+const WelcomeCart = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default WelcomeCart;
