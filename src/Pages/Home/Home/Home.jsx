cd742f382428bedb5cb2e2950565738e65c3522b
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-06-09 09:36:54 +0600

    creat the isntructores page

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "./Banner/Banner";
+import Instructors from "./Instructors/Instructors";
 
 const Home = () => {
     return (
         <div className="">
            <Banner></Banner>
+           <Instructors></Instructors>
         </div>
     );
 };
---
+++ src/Pages/Home/Home/Instructors/Instructors.jsx
@@ -0,0 +1,14 @@
+import { Helmet } from "react-helmet";
+import SectionTitle from "../../../../Component/SectioneTitle/SectionTitle";
+
+const Instructors = () => {
+    return (
+        <div>
+            <Helmet><title>Sadiq | Instructors</title></Helmet>
+            <SectionTitle></SectionTitle>
+            
+        </div>
+    );
+};
+
+export default Instructors;
