169155f0834b03d3c4a77c1d4959c603739fdb93
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-06-10 15:05:02 +0600

    added the all instructor section title

--- src/Component/ShearBanner/ShearBanner.jsx
+++ src/Component/ShearBanner/ShearBanner.jsx
@@ -2,13 +2,14 @@ import banner from '../../assets/sheareBanner.avif'
 
 
 
-const ShearBanner = ({title}) => {
+const ShearBanner = ({title,subtitle}) => {
     return (
         <div className="hero h-[600px] mb-20  " style={{ backgroundImage:
             `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(${banner})`}}>
             <div className="hero-content text-center mt-10 py-20 text-neutral-content  bg-[#00000052] w-5/6">
                 <div className=" text-black">
                     <h1 className="mb-5 text-5xl font-bold text-white">{title}</h1>
+                    <p className='text-amber-400'>{subtitle}</p>
                 </div>
             </div>
         </div>
--- src/Pages/Instructors/Instructors.jsx
+++ src/Pages/Instructors/Instructors.jsx
@@ -1,66 +1,19 @@
 import { Helmet } from "react-helmet";
 import ShearBanner from "../../Component/ShearBanner/ShearBanner";
+import SectionTitle from "../../Component/SectioneTitle/SectionTitle";
 
 const Instructors = () => {
     return (
         <div>
              <Helmet><title>Sadiq | instructores</title></Helmet>
-            <ShearBanner title={'Our instructores'}></ShearBanner>
+            <ShearBanner title={'Our instructores'} subtitle='All instructires page'></ShearBanner>
+            <h2 className="text-4xl font-bold text-center mt-24">All <span className="text-amber-400">Instructors</span></h2>
+            <SectionTitle subHeadding={''}></SectionTitle>
+            
         </div>
     );
 };
 
 export default Instructors;
 
-
-//  [
-//     {
-//       "_id": 1,
-//       "image": "https://i.ibb.co/hs05Jqk/351041039-653153336283840-1301765290027760543-n.png",
-//       "name": "MatheMatics Class",
-//       "Instructor name": "John Smith",
-//       "Available seats": 15,
-//       "Price": 10.99
-//     },
-//     {
-//       "_id": 2,
-//       "image": "https://i.ibb.co/mFw99R2/338374640-613958723881479-9133193691522719198-n.jpg",
-//       "name": "Chemistry Class",
-//       "Instructor name": "Jane Johnson",
-//       "Available seats": 12,
-//       "Price": 8.99
-//     },
-//     {
-//       "_id": 3,
-//       "image": "https://i.ibb.co/j6QbN3d/353132668-235580915857641-4659349977693052034-n.png",
-//       "name": "Physics Class",
-//       "Instructor name": "Michael Davis",
-//       "Available seats": 8,
-//       "Price": 12.99
-//     },
-//     {
-//       "_id": 4,
-//       "image": "https://i.ibb.co/qRbB3CG/350267781-617159070363770-3878914488958334269-n.png",
-//       "name": "English Class",
-//       "Instructor name": "Emily Wilson",
-//       "Available seats": 10,
-//       "Price": 9.99
-//     },
-//     {
-//       "_id": 5,
-//       "image": "https://i.ibb.co/52sx8wD/348886677-934227647653264-4526355407490032701-n.png",
-//       "name": "KBokhari Class",
-//       "Instructor name": "David Brown",
-//       "Available seats": 15,
-//       "Price": 11.99
-//     },
-//     {
-//       "_id": 6,
-//       "image": " https://i.ibb.co/sWN0fg3/353113214-953532942438387-1532505560779015007-n.png",
-//       "name": "History",
-//       "Instructor name": "Sarah Anderson",
-//       "Available seats": 20,
-//       "Price": 14.99
-//     }
-//   ];
   
