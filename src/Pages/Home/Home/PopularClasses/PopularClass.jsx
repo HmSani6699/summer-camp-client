469e257880b93334ce037adba14023ec6d637603
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-06-10 15:40:00 +0600

    style the cart select btn

--- src/Pages/Home/Home/PopularClasses/PopularClass.jsx
+++ src/Pages/Home/Home/PopularClasses/PopularClass.jsx
@@ -1,22 +1,21 @@
 
 const PopularClass = ({ classe }) => {
-    const { image,name, } = classe;
+    const { image, name, InstructorName, Price } = classe;
 
-
+    // console.log(classe);
     return (
         <div className="card  w-full border bg-base-100 shadow-xl">
             <div className="">
                 <figure><img className="h-[320px] rounded-t-xl border w-full" src={image} alt="Shoes" /></figure>
             </div>
-            <div className="card-body ">
-                <div className="flex items-center justify-between">
-                    <div >
-                        <h2 className="card-title">{name}</h2>
-                        <p><span className="font-bold">Email:</span></p>
-                    </div>
-                    <div className="border bg-amber-300 h-8 w-8 rounded-full flex items-center justify-center">
-                        <button className="btn">Add</button>
-                    </div>
+            <div className="card-body">
+                <div className="text-center">
+                    <h2 className="text-2xl font-bold text-center ">{name}</h2>
+                    <p><span className="font-bold"> Instructor Name:</span> {InstructorName}</p>
+                    <p><span className="font-bold">Price : </span><span className="text-amber-400 font-semibold">${Price}</span></p>
+                </div>
+                <div className="mt-4 ">
+                <button className="btn btn-outline btn-warning w-full">Warning</button>
                 </div>
             </div>
         </div>
--- src/Pages/Instructors/Instructors.jsx
+++ src/Pages/Instructors/Instructors.jsx
@@ -9,7 +9,6 @@ const Instructors = () => {
             <ShearBanner title={'Our instructores'} subtitle='All instructires page'></ShearBanner>
             <h2 className="text-4xl font-bold text-center mt-24">All <span className="text-amber-400">Instructors</span></h2>
             <SectionTitle subHeadding={''}></SectionTitle>
-            
         </div>
     );
 };
