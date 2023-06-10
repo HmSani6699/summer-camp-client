18240cb849fc9a387ed0ba22954fab665de7a891
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-06-10 12:40:17 +0600

    load the banner img

--- src/Component/ShearBanner/ShearBanner.jsx
+++ src/Component/ShearBanner/ShearBanner.jsx
@@ -1,8 +1,15 @@
+import banner from '../../assets/shearBanner.jpg'
 
-const ShearBanner = () => {
+
+
+const ShearBanner = ({title}) => {
     return (
-        <div>
-            
+        <div className="hero mt-10 min-h-screen mb-20  bg-fixed" style={{ backgroundImage: `url(${banner})` }}>
+            <div className="hero-content text-center py-20 text-neutral-content  bg-[#00000030] w-5/6">
+                <div className=" text-black">
+                    <h1 className="mb-5 text-5xl font-bold text-amber-400">{title}</h1>
+                </div>
+            </div>
         </div>
     );
 };
