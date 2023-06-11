576311ceb6160aa8de0b7c442534352fa5919fc2
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-06-11 16:21:54 +0600

    crat the selects calass component

--- src/Hook/useClass.jsx
+++ src/Hook/useClass.jsx
@@ -5,8 +5,7 @@ const useClass = () => {
     const { user } = useAuth()
 
     const { data: selectClass, refetch } = useQuery({
-
-        queryKey: ['carts', user?.email],
+        queryKey: ['class', user?.email],
         enabled: !!user?.email,
         queryFn: async () => {
             const res = await fetch(`http://localhost:5000/carts?email=${user?.email}`);
