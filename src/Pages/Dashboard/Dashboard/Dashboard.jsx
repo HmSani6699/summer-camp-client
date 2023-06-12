c6126be43fef349674b32501da386da20a6a86bb
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-06-12 15:48:06 +0600

    delete the auher info in admin menu

--- src/Pages/Dashboard/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard/Dashboard.jsx
@@ -6,6 +6,8 @@ const Dashboard = () => {
 
     const { user } = useAuth();
 
+    const isAdmin = true;
+
     return (
         <div className="w-full">
             <div className="drawer lg:drawer-open">
@@ -25,12 +27,19 @@ const Dashboard = () => {
                             </div>
                         </div>
                         <p className="mb-10 text-center">{user?.email}</p>
-                        {/* Sidebar content here */}
-                        <li><NavLink to="/dashboard/userHome"><FaHome className="text-[20px]"></FaHome> Student Home</NavLink></li>
-                        <li><NavLink to="/dashboard/selectClass"><FaBible className="text-[20px]"></FaBible>My Selected Classes</NavLink></li>
-                        <li><NavLink to="/dashboard/history"><FaTerminal className="text-[20px]"></FaTerminal> My Enrolled Classes</NavLink></li>
+                        {
+                            isAdmin ? <>
+                                <li><NavLink to="/dashboard/home"><FaHome></FaHome> ADMIN HOME</NavLink></li>
+                                <li><NavLink to="/dashboard/allusers"><FaUsers></FaUsers> Manage Users</NavLink></li>
+                            </> : <>
+                                <li><NavLink to="/dashboard/userHome"><FaHome className="text-[20px]"></FaHome> Student Home</NavLink></li>
+                                <li><NavLink to="/dashboard/selectClass"><FaBible className="text-[20px]"></FaBible>My Selected Classes</NavLink></li>
+                                <li><NavLink to="/dashboard/history"><FaTerminal className="text-[20px]"></FaTerminal> My Enrolled Classes</NavLink></li>
+                            </>
+                        }
 
                         <div className="border my-8 border-amber-500"></div>
+
                         <li><NavLink to="/"><FaHome className="text-[20px]"></FaHome> Home</NavLink> </li>
                         <li><NavLink to="/classes"><FaSchool className="text-[20px]"></FaSchool> Classes</NavLink></li>
                         <li><NavLink to="/instructors"><FaUsers className="text-[20px]"></FaUsers>Instructors</NavLink></li>
