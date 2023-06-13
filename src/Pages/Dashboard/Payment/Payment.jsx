beddad0ee124f1bcbca7233ab7588d4f61ebb06c
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-06-13 19:41:14 +0600

    added to stripe key

--- src/Pages/Dashboard/Payment/Payment.jsx
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -1,8 +1,17 @@
+import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "./CheckoutForm";
+import { loadStripe } from "@stripe/stripe-js";
+
+
+const stripePromise = loadStripe(import.meta.env.VITE_PAYMENT_GATEWAY_PK);
 
 const Payment = () => {
     return (
         <div>
             <h2> Taka amar nasa thka amar pasa</h2>
+            <Elements stripe={stripePromise}>
+                <CheckoutForm />
+            </Elements>
         </div>
     );
 };
