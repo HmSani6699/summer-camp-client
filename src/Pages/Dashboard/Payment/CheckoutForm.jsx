7968621c428a13124a9110fc8e48eaace2fe2d10
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-06-13 20:13:46 +0600

    set style the pay btn

--- src/Pages/Dashboard/Payment/CheckoutForm.jsx
+++ src/Pages/Dashboard/Payment/CheckoutForm.jsx
@@ -1,6 +1,8 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useState } from "react";
 
 const CheckoutForm = () => {
+    const [cartError, setCartError] = useState('')
 
     const stripe = useStripe();
     const elements = useElements()
@@ -17,31 +19,48 @@ const CheckoutForm = () => {
         if (card == null) {
             return;
         }
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
+            card,
+        });
+        if (error) {
+            console.log('[error]', error);
+            setCartError(error.message)
+
+        } else {
+            setCartError('')
+            console.log('[PaymentMethod]', paymentMethod);
+        }
     }
 
 
     return (
-        <form onSubmit={handleSubmit}>
-            <CardElement
-                options={{
-                    style: {
-                        base: {
-                            fontSize: '16px',
-                            color: '#424770',
-                            '::placeholder': {
-                                color: '#aab7c4',
+        <>
+            <form className="lg:w-[500px]" onSubmit={handleSubmit}>
+                <CardElement
+                    options={{
+                        style: {
+                            base: {
+                                fontSize: '16px',
+                                color: '#424770',
+                                '::placeholder': {
+                                    color: '#aab7c4',
+                                },
+                            },
+                            invalid: {
+                                color: '#9e2146',
                             },
                         },
-                        invalid: {
-                            color: '#9e2146',
-                        },
-                    },
-                }}
-            />
-            <button type="submit" disabled={!stripe}>
-                Pay
-            </button>
-        </form>
+                    }}
+                />
+                <button className="btn btn-warning mt-6 w-2/3 " type="submit" disabled={!stripe}>
+                    Pay
+                </button>
+            </form>
+        {
+            cartError&&<p className="text-red-600  mt-4 ">{cartError}</p>
+        }
+        </>
     );
 };
 
