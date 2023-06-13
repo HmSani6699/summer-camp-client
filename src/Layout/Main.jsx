9024422bf31f82a71c8632424f1edfed660bcb26
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-06-13 15:33:06 +0600

    delete the them text

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -2,10 +2,13 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../Pages/Home/Home/Shared/Navbar/Navbar";
 import Footer from "../Pages/Home/Home/Shared/Footer/Footer";
 
+
+
 const Main = () => {
+
     return (
-        <div className="max-w-7xl  mx-auto">
-            <Navbar></Navbar>
+        <div className={`max-w-7xl  mx-auto`}>
+            <Navbar ></Navbar>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
