d727760113a4cf121b563458fe489bc005be5c51
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-06-18 22:23:27 +0600

    un comment the register field

--- src/Component/SectioneTitle/SectionTitle.jsx
+++ src/Component/SectioneTitle/SectionTitle.jsx
@@ -1,17 +1,17 @@
 
-// import logo from '../../assets/logo.png'
+import logo from '../../assets/logo.png'
 
-// const SectionTitle = ({ subHeadding }) => {
-//     return (
-//         <div className='flex items-center justify-center'>
-//             <div>
-//                 <div className='flex items-center justify-center'>
-//                     <img className='h-24 w-24' src={logo} alt="" />
-//                 </div>
-//                 <p className='w-1/2 mx-auto text-center'>{subHeadding}</p>
-//             </div>
-//         </div>
-//     );
-// };
+const SectionTitle = ({ subHeadding }) => {
+    return (
+        <div className='flex items-center justify-center'>
+            <div>
+                <div className='flex items-center justify-center'>
+                    <img className='h-24 w-24' src={logo} alt="" />
+                </div>
+                <p className='w-1/2 mx-auto text-center'>{subHeadding}</p>
+            </div>
+        </div>
+    );
+};
 
-// export default SectionTitle;
+export default SectionTitle;
